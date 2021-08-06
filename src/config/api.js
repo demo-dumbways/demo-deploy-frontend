8eb0ac47df32627176e7591a60b987e649da535c
Jody Septiawan <jody@192.168.1.2>
2021-08-06 23:17:21 +0700

    Update

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 export const API = () => {
-  const baseUrl = 'https://deploy-backend-demo.herokuapp.com/api/v1/';
+  const baseUrl = 'https://deploy-backend-demo.herokuapp.com/api/v1';
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
